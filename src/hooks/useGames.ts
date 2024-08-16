42596efcecbfafecb58a74e6302a3750cb1f62b5
bharath-chen <sscbarath@gmail.com>
2024-08-16 09:01:17 +0530

    Add emojis

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -11,8 +11,10 @@ export interface Game {
   id: number;
   name: string;
   background_image: string;
-  metacritic: number;
   parent_platforms: { platform: Platform }[];
+  metacritic: number;
+  rating_top: number;
+  rating: number;
 }
 
 const useGames = (gameQuery: GameQuery) =>
